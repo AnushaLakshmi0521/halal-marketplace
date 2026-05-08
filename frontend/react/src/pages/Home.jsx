32e4fccbae2b2f52e38b2153e18b43652c6660bf
anug26446 <anug26446@example.com>
2026-05-08 23:26:28 +0530

    fix image import typo and case issue

--- frontend/react/src/pages/Home.jsx
+++ frontend/react/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import green from "../assets/green1.jpg";   // ✅ NEW IMAGE
 import "./Home.css";
 
 /* IMAGES */
-import all from "../assets/categories/all.jpg";g
+import all from "../assets/categories/all.jpg";
 import meat from "../assets/categories/Meat.jpg";
 import organic from "../assets/categories/organic.jpg";
 import pantry from "../assets/categories/pantry.jpg";
