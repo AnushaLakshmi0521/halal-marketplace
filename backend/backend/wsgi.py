4c150654ff6d8fe8c106544fd16814a6428621f4
anug26446 <anug26446@example.com>
2026-05-09 08:50:42 +0530

    changes

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE',
-    'backend.backend.settings'
+    'backend..settings'
 )
 
 application = get_wsgi_application()
